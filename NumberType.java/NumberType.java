8ce7934314a299ec0b824015d306c234d64ca4ab
deepti <deeptiyatagiri@gmail.com>
2025-06-11 09:42:46 +0530

    save

---
+++ NumberType.java/NumberType.java
@@ -0,0 +1,9 @@
+public class NumberType{
+    public static void main(String[] args){
+        int num=20;
+        System.out.println("enter the number");
+        if(num>=0){
+            System.out.println("positive");
+        }
+    }
+}
